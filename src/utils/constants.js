f8d398f453b8d623f7e8f0ecfadaf6ee5cd2d65c
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-18 02:59:50 +0530

    new commit for cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-export const RES_ITEM_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.66500&lng=77.44770&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RES_ITEM_API="https://apnakitchenlive.onrender.com/dapi/restaurants/list/v5?lat=28.66500&lng=77.44770&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6691565&lng=77.45375779999999&restaurantId=";
+export const MENU_API="https://apnakitchenlive.onrender.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6691565&lng=77.45375779999999&restaurantId=";
 
 export const AboutImg="https://www.w3schools.com";

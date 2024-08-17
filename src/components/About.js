3e678df3da2a0847051892dbccc0587c8836d28a
Aditya Kumar Gupta <adityakumargupta2101@gmail.com>
2024-08-18 03:35:11 +0530

    new commit for cors

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import OurTeam from './OurTeam';
+
 const About = () => {
   return (
     <>
@@ -10,9 +10,9 @@ const About = () => {
       <h3>Hello! I'm Aditya Kumar Gupta</h3>
       <p>I'm a 2021-2025 Batch Information Technology student at ABES Engineering College in Ghaziabad, Uttar Pradesh. </p>
       <p>If you want to know more about me.</p>
-      <Link to="/contact">Click here</Link>
+      <Link to="https://mraadig.github.io/myportfolio/" target='/'>Click here</Link>
     </div>
-    <OurTeam/>
+
     </>
   );
 };
